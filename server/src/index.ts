01542cf5370ff51e6dd76e9ceee878c4df400072
kchen53 <kellychen@ufl.edu>
2023-02-02 12:31:13 -0500

    minor changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
+import mongoose from 'mongoose';
+
 const app = express();
+const dataBase = mongoose.connect("mongodb+srv://IndependentStudy:CIS4905@cluster0.tovs3lh.mongodb.net/?retryWrites=true&w=majority");
 
 app.get('/', (req: Request, res: Response) => {
     res.send("hello world");
