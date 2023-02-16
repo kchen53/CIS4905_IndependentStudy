29287f8bc30f8292b58851786016ad6450ab15ad
kchen53 <kellychen@ufl.edu>
2023-02-16 00:33:02 -0500

    added navbar, routing, login, and sign in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import { } from 'semantic-ui-react';
-import './App.css'
+import { useState } from 'react';
+
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import "./App.css"
+import Home from './pages/Home';
+import About from './pages/About';
+import NavBar from './components/navbar';
+import Contact from './pages/Contact';
+import Menu from './pages/Menu';
+import Login from './pages/Login';
+
 
 function App() {
-  return(
-  <div className="App">
-    <form>
-      <header>A start</header>
-      <input/>
-      <button color='white'>Search</button>
-    </form>
-    </div>
-  )
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Menu() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="p-3">
+      <Container>
+        <Navbar.Brand href="#home">Restaurant</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              {' '}
+              <Link className="text-decoration-none text-white" to="/">
+                Home
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              {' '}
+              <Link className="text-decoration-none text-white" to="/about">
+                About
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              {' '}
+              <Link className="text-decoration-none text-white" to="/contact">
+                Contact Us
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              {' '}
+              <Link className="text-decoration-none text-white" to="/menu">
+                Menu
+              </Link>
+            </Nav.Link>
+          </Nav>
+          <Nav className="gap-2">
+            <Nav.Link className="btn btn-primary" href="#"><Link className="text-decoration-none text-white" to="/login">
+                Login
+              </Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
