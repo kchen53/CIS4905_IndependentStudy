71baf68a573b29ca2342180b53c1c777bef6440f
kchen53 <kellychen@ufl.edu>
2023-03-02 11:59:39 -0500

    menu component

--- client/src/pages/Menu.tsx
+++ client/src/pages/Menu.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
-export default function Menu() {
-  return (
-    <div>
-      <h1>Menu</h1>
-    </div>
-  )
+interface MenuItem {
+  id: number;
+  name: string;
+  price: number;
 }
+
+function Menu() {
+  const [menu, setMenu] = useState<MenuItem[]>([]);
+
+  useEffect(() => {
+    fetch('/menu')
+      .then((res) => res.json())
+      .then((data) => setMenu(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        {menu.map((item) => (
+          <Col key={item.id} md={4}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+                <Card.Text>${item.price}</Card.Text>
+                <Button variant="primary">Order</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+export default Menu;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,26 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
 const app = express();
 const dataBase = mongoose.connect("mongodb+srv://IndependentStudy:CIS4905@cluster0.tovs3lh.mongodb.net/?retryWrites=true&w=majority");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send("hello world");
+});
+
+app.get('/menu', (req: Request, res: Response) => {
+  const menu = [
+    { id: 1, name: 'Burger', price: 10 },
+    { id: 2, name: 'Pizza', price: 12 },
+    { id: 3, name: 'Pasta', price: 8 },
+  ];
+
+  res.json(menu);
 });
 
 app.listen(5000);
