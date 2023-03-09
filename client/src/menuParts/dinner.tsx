57cfc49b831f1ce6c67d8b17bfb9e7286e1620e7
kchen53 <kellychen@ufl.edu>
2023-03-09 13:41:59 -0500

    able to upload to database and display menu cards (post & get)

--- client/src/menuParts/dinner.tsx
+++ client/src/menuParts/dinner.tsx
@@ -6,7 +6,7 @@ import Card, { CardGroup, Col, Row } from 'react-bootstrap'
 function dinnerMenu() {
   return (
     <Row xs={1} md={6} className="g-4">
-      {MenuCards("food", "24", Image, "hi")}
+      {/* {MenuCards("food", "24", Image, "hi")}
       {MenuCards("food", "25", Image, "hi")}
       {MenuCards("food", "26", Image, "hi")}
       {MenuCards("food", "27", Image, "hi")}
@@ -19,7 +19,7 @@ function dinnerMenu() {
       {MenuCards("food", "27", Image, "hi")}
       {MenuCards("food", "28", Image, "hi")}
       {MenuCards("food", "29", Image, "hi")}
-      {MenuCards("food", "30", Image, "hi")}
+      {MenuCards("food", "30", Image, "hi")} */}
     </Row>
     
   );
